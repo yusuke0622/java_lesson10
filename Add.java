139c2668053c61eca1942d06fb332d7a857ea887
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2024-01-08 14:52:27 +0000

    first commit

---
+++ Add.java
@@ -0,0 +1,14 @@
+public class Add implements Calc {
+   
+    public void method1() {
+        System.out.println(str + "をします");
+    }
+    
+    public void method2(String str) {
+        System.out.println(str + "をします")
+    }
+    
+    public void method3() {
+        System.out.println("計算結果:" + (a + b));
+    }
+}
---
+++ Calc.java
@@ -0,0 +1,11 @@
+public interface Calc {
+    
+    public String str = "足し算";
+    public int a = 3;
+    public int b = 6;
+    
+    void method1();
+    void method2(String str);
+    void method3();
+    
+}
